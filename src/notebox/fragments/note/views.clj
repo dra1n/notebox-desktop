5dc524db0a3e931de80f5ed9b7d7d81b0e7f53b1
v-andreyb <v-andreyb@zillowgroup.com>
2022-05-04 12:15:42 +0300

    Add scroll to note content

--- src/notebox/fragments/note/views.clj
+++ src/notebox/fragments/note/views.clj
@@ -1,21 +1,25 @@
 (ns notebox.fragments.note.views)
 
 (defn note [{:keys [note]}]
-  {:fx/type :v-box
-   :style-class "note-content"
-   :children [{:fx/type :text-flow
-               :style-class "note-title-wrapper"
-               :pref-width 400
-               :children [{:fx/type :text
-                           :style-class "note-title"
-                           :cache true
-                           :cache-hint :speed
-                           :text (:title note)}]}
-              {:fx/type :text-flow
-               :style-class "note-text-wrapper"
-               :pref-width 400
-               :children [{:fx/type :text
-                           :style-class "note-text"
-                           :cache true
-                           :cache-hint :speed
-                           :text (:text note)}]}]})
+  {:fx/type :scroll-pane
+   :style-class "scroll-pane"
+   :fit-to-width true
+   :fit-to-height true
+   :content {:fx/type :v-box
+             :style-class "note-content"
+             :children [{:fx/type :text-flow
+                         :style-class "note-title-wrapper"
+                         :pref-width 400
+                         :children [{:fx/type :text
+                                     :style-class "note-title"
+                                     :cache true
+                                     :cache-hint :speed
+                                     :text (:title note)}]}
+                        {:fx/type :text-flow
+                         :style-class "note-text-wrapper"
+                         :pref-width 400
+                         :children [{:fx/type :text
+                                     :style-class "note-text"
+                                     :cache true
+                                     :cache-hint :speed
+                                     :text (:text note)}]}]}})
